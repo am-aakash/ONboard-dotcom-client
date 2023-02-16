814bf42e17f94dca6b1f01a6ed37362d8016c8d0
Aakash Chaudhary <akashchaudhary12121@gmail.com>
2023-02-16 18:19:52 +0530

    Navbar changed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 export const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="./">
                     Onboard.com
