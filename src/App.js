4a810fbe57b8223187350397a008d8fa413184f7
Aakash Chaudhary <akashchaudhary12121@gmail.com>
2023-02-17 19:10:10 +0530

    Warning removed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import "./App.css"
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Navbar } from "./components/navbar"
 import { Home } from "./components/employee_components/home"
 import { About } from "./components/about"
