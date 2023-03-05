e3fe4fdac36f5d16355329068f629f0419c74936
Ritika <ritikapatel290@gmail.com>
2023-03-05 12:01:14 +0530

    Employee_details Responsive screen

--- src/components/employee_components/employee_details/employeeDetails.js
+++ src/components/employee_components/employee_details/employeeDetails.js
@@ -13,28 +13,30 @@ const EmployeeDetails = (props) => {
         marginTop: '5rem',
       }}
     >
-      <div className="row">
-        <div className="col">
-          <div style={{ display: 'flex' }}>
-            <div className="profilePicBig">
-              <img src={require('../../../assets/avatar.jpg')} alt="avatar" />
-            </div>
-            <div style={{ marginLeft: '1.2rem', marginTop: '3.2rem', width: '20rem' }}>
-              <h2 style={{ fontWeight: '600' }}>Test Employee Name</h2>
-              <h5 style={{ fontWeight: '500', marginTop: '0.6rem', marginLeft: '0.2rem' }}>Software Developent Engineer-2</h5>
-              <h6 style={{ color: 'rgb(84, 123, 230)', fontWeight: '600', marginTop: '0.6rem', marginLeft: '0.2rem' }}>Company Name | Engineering</h6>
-              <h6 style={{ color: 'rgb(124, 124, 124)', fontWeight: '600', marginTop: '0.6rem', marginLeft: '0.2rem' }}> Date of Joining : 14 Apr 2023 </h6>
-            </div>
-          </div>
+      <div className="rowR">
+        <div className="profilePicBig">
+          <img src={require('../../../assets/avatar.jpg')} alt="avatar" />
         </div>
-        <div class="col">
-          <div style={{ marginLeft: '18rem', marginTop: '4rem' }}>
+
+      <div className='d'>
+        <div className="d1" style={{marginTop: '3.2rem' }}>
+          <h2 style={{ fontWeight: '600' }}>Test Employee Name</h2>
+          <h5 style={{ fontWeight: '500', marginTop: '0.6rem', marginLeft: '0.2rem' }}>Software Developent Engineer-2</h5>
+          <h6 style={{ color: 'rgb(84, 123, 230)', fontWeight: '600', marginTop: '0.6rem', marginLeft: '0.2rem' }}>Company Name | Engineering</h6>
+          <h6 style={{ color: 'rgb(124, 124, 124)', fontWeight: '600', marginTop: '0.6rem', marginLeft: '0.2rem' }}> Date of Joining : 14 Apr 2023 </h6>
+        </div>
+
+        <div className="d2">
+          <div className="abc">
             <h6 style={{ color: 'rgb(84, 84, 84)', fontWeight: '500', fontSize: 'small' }}> +91-7803436675</h6>
             <h6 style={{ color: 'rgb(84, 84, 84)', fontWeight: '500', fontSize: 'small' }}> testemployee@email.com </h6>
-            <h6 style={{ color: 'rgb(84, 84, 84)', fontWeight: '500', fontSize: 'small'  }}> Employee Id: 254367767 </h6>
+            <h6 style={{ color: 'rgb(84, 84, 84)', fontWeight: '500', fontSize: 'small' }}> Employee Id: 254367767 </h6>
           </div>
         </div>
+
       </div>
+      </div>
+
       <CurrentStatusCard />
       <StatusHistoryCard />
       <OnboardingSetup />
